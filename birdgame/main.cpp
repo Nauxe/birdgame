b76dbade53f61b95eedef4baef644554419785aa
Nauxe <lexuanyu271828@gmail.com>
2026-07-13 16:46:12 +0800

    Add <math.h> include

--- birdgame/main.cpp
+++ birdgame/main.cpp
@@ -3,6 +3,7 @@
 #include "textures/textures.h"
 #include "utils/utils.h"
 #include <memory>
+#include <math.h>
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
