579b44269e8ca24cfa10bbbdebe649d116d1db92
Nauxe <lexuanyu271828@gmail.com>
2026-07-13 16:56:40 +0800

    Add `#include <climits>`

--- birdgame/main.cpp
+++ birdgame/main.cpp
@@ -4,6 +4,7 @@
 #include "utils/utils.h"
 #include <memory>
 #include <math.h>
+#include <climits>
 #define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
